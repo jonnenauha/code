ea274a1b705d86ce37a1311dd52c7946772e764a
Ryan McDougall <ryanm@ryanm-laptop.(none)>
2009-06-18 22:46:09 +0300

    make it do something

--- xrd/main.cs
+++ xrd/main.cs
@@ -13,29 +13,27 @@ public class XRDParseException : System.Exception
 
 public class XRDParser
 {
-    public class XRDDocument
+    public class XRDLink 
     {
-        public class XRDLink 
+        internal string relation_;
+        internal string uri_template_;
+        internal Uri uri_;
+        internal string media_type_;
+
+        internal XRDLink (string relation, Uri uri, string mediatype)
         {
-            internal string relation_;
-            internal string uri_template_;
-            internal Uri uri_;
-            internal string media_type_;
-
-            XRDLink (string relation, string uritemplate, Uri uri, string mediatype)
-            {
-                relation_ = relation;
-                uri_template_ = uritemplate;
-                uri_ = uri;
-                media_type_ = mediatype;
-            }
-
-            public string Relation { get { return relation_; } }
-            public string UriTemplate { get { return uri_template_; } }
-            public Uri Uri { get { return uri_; } }
-            public string MediaType { get { return media_type_; } }
+            relation_ = relation;
+            uri_ = uri;
+            media_type_ = mediatype;
         }
 
+        public string Relation { get { return relation_; } }
+        public Uri Uri { get { return uri_; } }
+        public string MediaType { get { return media_type_; } }
+    }
+
+    public class XRDDocument
+    {
         internal string expires_; //TODO: should be a time data type
         internal string subject_; 
         internal List <string> aliases_ = new List <string> (); 
@@ -59,6 +57,9 @@ public class XRDParser
     private XPathExpression subject_exp_;
     private XPathExpression aliases_exp_;
     private XPathExpression types_exp_;
+    private XPathExpression link_rel_exp_;
+    private XPathExpression link_uri_exp_;
+    private XPathExpression link_mediatype_exp_;
     
     public XRDParser (Stream xrd)
     {
@@ -70,6 +71,10 @@ public class XRDParser
         subject_exp_ = cursor_.Compile ("/XRD/Subject");
         aliases_exp_ = cursor_.Compile ("/XRD/Alias");
         types_exp_ = cursor_.Compile ("/XRD/Type");
+
+        link_rel_exp_ = cursor_.Compile ("/XRD/Link/Rel");
+        link_uri_exp_ = cursor_.Compile ("/XRD/Link/URI");
+        link_mediatype_exp_ = cursor_.Compile ("/XRD/Link/MediaType");
     }
 
     public XRDDocument Document
@@ -88,13 +93,25 @@ public class XRDParser
         var aliases = get_all_ (cursor_.Select (aliases_exp_));
         var types = get_all_ (cursor_.Select (types_exp_));
 
+        var rel = get_all_ (cursor_.Select (link_rel_exp_));
+        var uri = get_all_ (cursor_.Select (link_uri_exp_));
+        var mediatype = get_all_ (cursor_.Select (link_mediatype_exp_));
+
         if ((expires.Count != 1) || (subject.Count != 1) || (aliases.Count < 1) || (types.Count < 1)) 
-            throw new XRDParseException ("incorrect number of tags");
+            throw new XRDParseException ();
 
         result_.expires_ = expires[0];
         result_.subject_ = subject[0];
         result_.aliases_ = aliases;
         result_.types_ = types;
+
+        if ((rel.Count != uri.Count) || (uri.Count != mediatype.Count))
+            throw new XRDParseException ();
+
+        int nlinks = rel.Count;
+        for (int i=0; i < nlinks; ++i)
+            result_.links_.Add 
+                (new XRDLink (rel[i], new Uri (uri[i]), mediatype[i]));
     }
 
     private List <string> get_all_ (XPathNodeIterator iter)
@@ -110,10 +127,19 @@ public class XRDParser
         var xrdfile = new FileStream ("xrd.xml", FileMode.Open);
         var parser = new XRDParser (xrdfile);
         var doc = parser.Document;
+        var links = doc.Links;
 
         Console.WriteLine ("expires: " + doc.Expires);
         Console.WriteLine ("subject: " + doc.Subject);
         foreach (string s in doc.Aliases) Console.WriteLine ("aliases: " + s);
         foreach (string s in doc.Types) Console.WriteLine ("types: " + s);
+
+        for (int i=0; i < links.Count; ++i)
+        {
+            Console.WriteLine ("link {0} rel: {1}", i, links[i].Relation);
+            Console.WriteLine ("link {0} uri: {1}", i, links[i].Uri);
+            Console.WriteLine ("link {0} mediatype: {1}", i, links[i].MediaType);
+        }
+
     }
 }
