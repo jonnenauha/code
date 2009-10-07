42ed5bf872f2fe41a0eb61d0967fda6379a263de
Ryan McDougall <ryanm@localhost.localdomain>
2009-10-07 12:21:36 +0300

    qt-ogre-test update

--- qt-ogre-test/main.cpp
+++ qt-ogre-test/main.cpp
@@ -64,52 +64,185 @@ Ogre::Root *render_setup (int width, int height)
     return root;
 }
 
+
 //=============================================================================
 //
-SceneManager::SceneManager (Ogre::Root *scene_root)
+OgreWidget::OgreWidget (Ogre::Root *root, QWidget *parent)
+: QWidget (parent), root_ (root), win_ (NULL)
 {
-    mainwin = new QWidget;
-    mainlay = new QVBoxLayout;
-
-    worldscene = new TestWidget (scene_root, mainwin);
-
-    mainpainter = new QPainter (worldscene);
-
-    uiscene = new QGraphicsScene;
-    uiview = new RedirectedQGraphicsView (mainpainter, uiscene);
-
-    line = new QLineEdit;
-    line-> setText ("wtf");
-
-    mainlay-> addWidget (worldscene);
-    mainwin-> setLayout (mainlay);
-    mainwin-> setMinimumSize (100, 100);
-
-    uiscene-> addWidget (line);
-
-    worldscene-> installEventFilter (this);
-    uiscene-> installEventFilter (this);
-    uiview-> installEventFilter (this);
-
-    uiview-> Realize ();
-
-    startTimer (20);
+    setAttribute (Qt::WA_PaintOnScreen);
+    setAttribute (Qt::WA_PaintOutsidePaintEvent);
+    setAttribute (Qt::WA_NoSystemBackground);
+    createRenderWindow ();
 }
 
-bool SceneManager::eventFilter (QObject *o, QEvent *e)
+void OgreWidget::Update ()
 {
-    // paint manually in the timer to avoid flicker
-    if (e-> type() == QEvent::Paint)
-        return true;
-
-    // forward to uiview
-    if (o == worldscene)// && e-> spontaneous())
+    if (win_) 
     {
-        cout << "forwarded: " << e-> type() << endl;
-        QApplication::sendEvent (uiview, e);
+        root_-> _fireFrameStarted();
+        win_-> update();
+        root_-> _fireFrameEnded();
     }
-    
-    return false;
+}
+
+void OgreWidget::createRenderWindow ()
+{
+    if (win_) return;
+
+    Ogre::NameValuePairList params;
+
+#ifndef Q_WS_WIN
+    QX11Info info = x11Info();
+    Ogre::String winhandle;
+
+    winhandle = Ogre::StringConverter::toString 
+        (reinterpret_cast<unsigned long> 
+         (info.display()));
+    winhandle += ":";
+
+    winhandle += Ogre::StringConverter::toString 
+        (static_cast<unsigned int> 
+         (info.screen()));
+    winhandle += ":";
+
+    winhandle += Ogre::StringConverter::toString 
+        (static_cast<unsigned long> 
+         (parentWidget()? 
+          parentWidget()->winId() : winId()));
+
+    params["parentWindowHandle"] = winhandle;
+#else
+    params["externalWindowHandle"] = 
+        Ogre::StringConverter::toString
+        ((size_t)(HWND) winId());
+#endif
+
+    win_ = root_-> createRenderWindow ("View", width(), height(), false, &params);
+
+    // take over ogre window
+    // needed with parent windows
+    if (parentWidget())
+    {
+        WId ogre_winid = 0x0;
+        win_-> getCustomAttribute ("WINDOW", &ogre_winid);
+        assert (ogre_winid);
+        create (ogre_winid);
+    }
+}
+
+void OgreWidget::resizeRenderWindow ()
+{
+    win_-> resize (width(), height());
+    win_-> windowMovedOrResized ();
+}
+
+void TestWidget::mousePressEvent (QMouseEvent *e) 
+{
+    mousepos = e-> pos();
+    if (mainnode) orientation = mainnode-> getOrientation();
+    mousepressed = true;
+}
+
+void TestWidget::mouseReleaseEvent (QMouseEvent *e) 
+{ 
+    mousepressed = false; 
+}
+
+void TestWidget::mouseMoveEvent (QMouseEvent *e) 
+{
+    if (mousepressed) 
+    {
+        QPoint curPos = e-> pos();
+
+        double w = width();
+        double h = height();
+
+        double curX = (curPos.x() * 2. - w) / w;
+        double curY = (h - curPos.y() * 2.) / h;
+        double x0 = (mousepos.x() * 2. - w) / w;
+        double y0 = (h - mousepos.y() * 2.) / h;
+
+        Ogre::Vector3 v1(x0, y0, 0);
+        Ogre::Vector3 v2(curX, curY, 0);
+
+        double radiusSqr = 0.8 * 0.8;
+        double cutoff = radiusSqr * 0.5;
+        double Rho = v1[0] * v1[0] + v1[1] * v1[1];
+        v1[2] = (Rho < cutoff) ? sqrt(radiusSqr - Rho) : (cutoff / sqrt(Rho));
+
+        Rho = v2[0] * v2[0] + v2[1] * v2[1];
+        v2[2] = (Rho < cutoff) ? sqrt(radiusSqr - Rho) : (cutoff / sqrt(Rho));
+
+        // v_cross is the normal of rotating plane
+        Ogre::Vector3 cross = v2.crossProduct(v1);
+        cross.normalise();
+
+        // compute the angle
+        v1.normalise();
+        v2.normalise();
+        double cosAngle = v1.dotProduct(v2);
+        if (cosAngle < -1.0)
+            cosAngle = -1.0;
+        else if(cosAngle > 1.0)
+            cosAngle = 1.0;
+        double angle = acos(cosAngle);
+
+        mainnode->rotate(cross, Ogre::Radian(angle));
+
+        mousepos = curPos;
+        orientation = mainnode->getOrientation();
+    }
+}
+
+void TestWidget::resizeEvent (QResizeEvent *e)
+{
+    OgreWidget::resizeEvent (e);
+
+    if (win_) 
+    {
+        cam-> setAspectRatio (Ogre::Real(width()) / Ogre::Real(height()));
+        viewport-> update();
+    }
+}
+
+//=============================================================================
+//
+TestWidget::TestWidget (Ogre::Root *root, QWidget *parent)
+: OgreWidget (root, parent)
+{
+    sceneman = root_->createSceneManager (Ogre::ST_GENERIC);
+
+    // Create the camera
+    cam = sceneman->createCamera ("PlayerCam");
+    cam->setPosition (Ogre::Vector3(0, 0, 200));
+
+    // Look back along -Z
+    cam->lookAt (Ogre::Vector3(0, 0, -300));
+    cam->setNearClipDistance (5);
+
+    // Create one viewport, entire window
+    viewport = win_->addViewport (cam);
+    viewport->setBackgroundColour (Ogre::ColourValue(0,0,0));
+    viewport->setClearEveryFrame (true);
+
+    sceneman->setAmbientLight (Ogre::ColourValue(0.6, 0.6, 0.6));
+
+    // Setup the actual scene
+    Ogre::Light* l = sceneman->createLight ("MainLight");
+    l->setPosition(0, 100, 500);
+
+    Ogre::Entity* mesh = sceneman->createEntity ("mesh", "dwarf.mesh");
+    mainnode = sceneman->getRootSceneNode()->createChildSceneNode();
+    mainnode->attachObject (mesh);
+
+    cam->setAutoTracking (true, mainnode);
+
+    Ogre::MaterialManager::getSingleton().setDefaultTextureFiltering (Ogre::TFO_BILINEAR);
+    Ogre::MaterialManager::getSingleton().setDefaultAnisotropy (1);
+
+    // Alter the camera aspect ratio to match the viewport
+    cam->setAspectRatio (Ogre::Real(viewport->getActualWidth()) / Ogre::Real(viewport->getActualHeight()));
 }
 
 //=============================================================================
--- qt-ogre-test/main.h
+++ qt-ogre-test/main.h
@@ -22,88 +22,22 @@ class OgreWidget : public QWidget
     Q_OBJECT
 
     public:
-        OgreWidget (Ogre::Root *root, QWidget *parent = 0)
-            : QWidget (parent), root_ (root), win_ (NULL)
-        {
-            setAttribute (Qt::WA_PaintOnScreen);
-            setAttribute (Qt::WA_PaintOutsidePaintEvent);
-            setAttribute (Qt::WA_NoSystemBackground);
-            createRenderWindow ();
-        }
-
+        OgreWidget (Ogre::Root *root, QWidget *parent = 0);
         ~OgreWidget () {}
 
         // forces rendering of a frame
-        void Update ()
-        {
-            if (win_) 
-            {
-                root_-> _fireFrameStarted();
-                win_-> update();
-                root_-> _fireFrameEnded();
-            }
-        }
+        void Update ();
 
     protected:
 
         // Qt overrides
         QSize minSizeHint () const { return QSize (100, 100); }
-        
         void paintEvent (QPaintEvent *e) { Update (); }
-
         void resizeEvent(QResizeEvent *e) { if (win_) resizeRenderWindow (); }
 
         // ogre-specific render window handling
-        void createRenderWindow ()
-        {
-            if (win_) return;
-
-            Ogre::NameValuePairList params;
-
-#ifndef Q_WS_WIN
-            QX11Info info = x11Info();
-            Ogre::String winhandle;
-
-            winhandle = Ogre::StringConverter::toString 
-                (reinterpret_cast<unsigned long> 
-                 (info.display()));
-            winhandle += ":";
-
-            winhandle += Ogre::StringConverter::toString 
-                (static_cast<unsigned int> 
-                 (info.screen()));
-            winhandle += ":";
-
-            winhandle += Ogre::StringConverter::toString 
-                (static_cast<unsigned long> 
-                 (parentWidget()? 
-                  parentWidget()->winId() : winId()));
-
-            params["parentWindowHandle"] = winhandle;
-#else
-            params["externalWindowHandle"] = 
-                Ogre::StringConverter::toString
-                ((size_t)(HWND) winId());
-#endif
-
-            win_ = root_-> createRenderWindow ("View", width(), height(), false, &params);
-
-            // take over ogre window
-            // needed with parent windows
-            if (parentWidget())
-            {
-                WId ogre_winid = 0x0;
-                win_-> getCustomAttribute ("WINDOW", &ogre_winid);
-                assert (ogre_winid);
-                create (ogre_winid);
-            }
-        }
-
-        void resizeRenderWindow ()
-        {
-            win_-> resize (width(), height());
-            win_-> windowMovedOrResized ();
-        }
+        void createRenderWindow ();
+        void resizeRenderWindow ();
 
         // implementation variables
         Ogre::RenderWindow  *win_;
@@ -116,116 +50,19 @@ class TestWidget : public OgreWidget
 
     public:
 
-        TestWidget (Ogre::Root *root, QWidget *parent = 0)
-            : OgreWidget (root, parent)
-        {
-            sceneman = root_->createSceneManager (Ogre::ST_GENERIC);
-
-            // Create the camera
-            cam = sceneman->createCamera ("PlayerCam");
-            cam->setPosition (Ogre::Vector3(0, 0, 200));
-
-            // Look back along -Z
-            cam->lookAt (Ogre::Vector3(0, 0, -300));
-            cam->setNearClipDistance (5);
-
-            // Create one viewport, entire window
-            viewport = win_->addViewport (cam);
-            viewport->setBackgroundColour (Ogre::ColourValue(0,0,0));
-            viewport->setClearEveryFrame (true);
-
-            sceneman->setAmbientLight (Ogre::ColourValue(0.6, 0.6, 0.6));
-
-            // Setup the actual scene
-            Ogre::Light* l = sceneman->createLight ("MainLight");
-            l->setPosition(0, 100, 500);
-
-            Ogre::Entity* mesh = sceneman->createEntity ("mesh", "dwarf.mesh");
-            mainnode = sceneman->getRootSceneNode()->createChildSceneNode();
-            mainnode->attachObject (mesh);
-
-            cam->setAutoTracking (true, mainnode);
-
-            Ogre::MaterialManager::getSingleton().setDefaultTextureFiltering (Ogre::TFO_BILINEAR);
-            Ogre::MaterialManager::getSingleton().setDefaultAnisotropy (1);
-
-            // Alter the camera aspect ratio to match the viewport
-            cam->setAspectRatio (Ogre::Real(viewport->getActualWidth()) / Ogre::Real(viewport->getActualHeight()));
-        }
+        TestWidget (Ogre::Root *root, QWidget *parent = 0);
+        ~TestWidget () {}
 
         void Update () { OgreWidget::Update (); }
 
     protected:
         
-        void mousePressEvent (QMouseEvent *e) 
-        {
-            mousepos = e-> pos();
-            if (mainnode) orientation = mainnode-> getOrientation();
-            mousepressed = true;
-        }
+        void mousePressEvent (QMouseEvent *e);
+        void mouseReleaseEvent (QMouseEvent *e);
+        void mouseMoveEvent (QMouseEvent *e);
         
-        void mouseReleaseEvent (QMouseEvent *) { mousepressed = false; }
-        
-        void mouseMoveEvent (QMouseEvent *e) 
-        {
-            if (mousepressed) 
-            {
-                QPoint curPos = e-> pos();
-
-                double w = width();
-                double h = height();
-
-                double curX = (curPos.x() * 2. - w) / w;
-                double curY = (h - curPos.y() * 2.) / h;
-                double x0 = (mousepos.x() * 2. - w) / w;
-                double y0 = (h - mousepos.y() * 2.) / h;
-
-                Ogre::Vector3 v1(x0, y0, 0);
-                Ogre::Vector3 v2(curX, curY, 0);
-
-                double radiusSqr = 0.8 * 0.8;
-                double cutoff = radiusSqr * 0.5;
-                double Rho = v1[0] * v1[0] + v1[1] * v1[1];
-                v1[2] = (Rho < cutoff) ? sqrt(radiusSqr - Rho) : (cutoff / sqrt(Rho));
-
-                Rho = v2[0] * v2[0] + v2[1] * v2[1];
-                v2[2] = (Rho < cutoff) ? sqrt(radiusSqr - Rho) : (cutoff / sqrt(Rho));
-
-                // v_cross is the normal of rotating plane
-                Ogre::Vector3 cross = v2.crossProduct(v1);
-                cross.normalise();
-
-                // compute the angle
-                v1.normalise();
-                v2.normalise();
-                double cosAngle = v1.dotProduct(v2);
-                if (cosAngle < -1.0)
-                    cosAngle = -1.0;
-                else if(cosAngle > 1.0)
-                    cosAngle = 1.0;
-                double angle = acos(cosAngle);
-
-                mainnode->rotate(cross, Ogre::Radian(angle));
-
-                mousepos = curPos;
-                orientation = mainnode->getOrientation();
-            }
-        }
-        
-        QSize minSizeHint () const { return QSize (100, 100); }
-
         void paintEvent (QPaintEvent *e) { Update (); }
-
-        void resizeEvent(QResizeEvent *e)
-        {
-            OgreWidget::resizeEvent (e);
-
-            if (win_) 
-            {
-                cam-> setAspectRatio (Ogre::Real(width()) / Ogre::Real(height()));
-                viewport-> update();
-            }
-        }
+        void resizeEvent (QResizeEvent *e);
 
         bool mousepressed;
         QPoint mousepos;
@@ -280,7 +117,35 @@ class SceneManager : public QObject
     Q_OBJECT
 
     public:
-        SceneManager (Ogre::Root *scene_root);
+        SceneManager (Ogre::Root *scene_root)
+        {
+            mainwin = new QWidget;
+            mainlay = new QVBoxLayout;
+
+            worldscene = new TestWidget (scene_root, mainwin);
+
+            mainpainter = new QPainter (worldscene);
+
+            uiscene = new QGraphicsScene;
+            uiview = new RedirectedQGraphicsView (mainpainter, uiscene);
+
+            line = new QLineEdit;
+            line-> setText ("wtf");
+
+            mainlay-> addWidget (worldscene);
+            mainwin-> setLayout (mainlay);
+            mainwin-> setMinimumSize (100, 100);
+
+            uiscene-> addWidget (line);
+
+            worldscene-> installEventFilter (this);
+            uiscene-> installEventFilter (this);
+            uiview-> installEventFilter (this);
+
+            uiview-> Realize ();
+
+            startTimer (20);
+        }
 
         void Start () { mainwin-> show(); }
 
@@ -294,7 +159,22 @@ class SceneManager : public QObject
 
         void timerEvent (QTimerEvent *e) { Update (); }
 
-        bool eventFilter (QObject *o, QEvent *e);
+        bool eventFilter (QObject *o, QEvent *e)
+        {
+            // paint manually in the timer to avoid flicker
+            if (e-> type() == QEvent::Paint)
+                return true;
+
+            // forward to uiview
+            if (o == worldscene)// && e-> spontaneous())
+            {
+                cout << "forwarded: " << e-> type() << endl;
+                QApplication::sendEvent (uiview, e);
+            }
+
+            return false;
+        }
+
 
         QWidget         *mainwin;
         QVBoxLayout     *mainlay;
