6c81b53cd21fd8ad9be0dac2bc1ce38e6d6ca7d2
Ryan McDougall <sempuki1@gmail.com>
2009-11-23 10:34:43 -0800

    update sagase and LF changes

--- sagase-cmake/Sagase.cmake
+++ sagase-cmake/Sagase.cmake
@@ -1,5 +1,3 @@
-# -- Ryan McDougall, realXtend -- 2009
-
 # parse a keyword delimited list of arguments into single list
 # results are in OUT
 function (sagase_parse_arguments BEGIN END ARGS)
@@ -44,14 +42,14 @@ function (sagase_generate_paths INCLUDE_PATHS LIBRARY_PATHS)
     #    set (normal_path_prefixes ${normal_path_prefixes} ${p})
     #endforeach ()
 
-    set (include_names "include" "includes")
-    set (lib_names "lib" "libs" "bin" "bins" "dll" "dlls")
+    set (include_names "." "include" "includes")
+    set (lib_names "." "lib" "libs" "bin" "bins" "dll" "dlls")
 
     # add prefix+name paths
     foreach (prefix ${path_prefixes})
         foreach (pkgname ${path_names})
 
-            foreach (inclname ${path_names})
+            foreach (inclname ${include_names})
                 set (includes ${includes} 
                     ${prefix}/${pkgname}/${inclname}
                     ${prefix}/${inclname}/${pkgname})
@@ -75,9 +73,8 @@ endfunction (sagase_generate_paths)
 # "NAMES" is a list of names by which the package is known. used by
 # find_package and in constructing search path names.
 # "COMPONENTS" is a list of sub-components that are required. used by
-# find_package and by pkg-config.
+# find_package and by brute-force.
 # "PREFIXES" is a list of path prefixes where the components might be found.
-# "SKIP_FIND_PACKAGE" causes it to bypass find_package
 # results are in ${PREFIX}_INCLUDE_DIRS, ${PREFIX}_LIBRARY_DIRS, 
 # ${PREFIX}_LIBRARIES, ${PREFIX}_DEFINITIONS, or fatal error.
 
@@ -88,7 +85,7 @@ endfunction (sagase_generate_paths)
 # 3. find_path 
 
 # "NAMES" and "COMPONENTS" mean slightly different things depending on the
-# seach methond. For find_package, consult the cmake documentation. For
+# search method. For find_package, consult the cmake documentation. For
 # pkg-config, only the "NAMES" are used. For find_path/brute force, "NAMES"
 # are possible directory names, and "COMPONENTS" are the possible header or
 # library filenames.
@@ -114,6 +111,8 @@ macro (sagase_configure_package PREFIX)
     sagase_parse_arguments ("COMPONENTS" "PREFIXES" PKG_COMPONENTS ${ARGN})
     sagase_parse_arguments ("PREFIXES" none PKG_PREFIXES ${ARGN})
 
+    message (STATUS "** Configuring " ${PREFIX})
+
     set (found_ FALSE)
 
     foreach (name_ ${PKG_NAMES})
@@ -122,18 +121,17 @@ macro (sagase_configure_package PREFIX)
 
         # find_package can't handle packages in all caps.
         # cmake is a macro language, which means it defines global variables,
-        # which it tries to namespace by choosing all caps variable names,
+        # which it tries to namespace by choosing all caps variable names.
         # when searching for a package who's name is all caps, this appears to
         # cause a name collision, and is thus bypassed.
         if (NOT name_ STREQUAL name_upper_)
-            message (STATUS "trying find_package: " ${name_})
-
+            
             # try built-in CMake modules first
             find_package (${name_} QUIET COMPONENTS ${PKG_COMPONENTS})
         endif ()
 
         if (${name_}_FOUND OR ${name_upper_}_FOUND)
-            message (STATUS "sagase: configured " ${PREFIX})
+            message (STATUS "sagase: configured " ${PREFIX} " using find_package")
             set (${PREFIX}_INCLUDE_DIRS ${${name_}_INCLUDE_DIRS})
             set (${PREFIX}_LIBRARIES ${${name_}_LIBRARIES})
             set (${PREFIX}_LIBRARY_DIRS ${${name_}_LIBRARY_DIRS})
@@ -154,8 +152,6 @@ macro (sagase_configure_package PREFIX)
             elseif (UNIX)
                 # what else is there besides pkg-config?
                 # non-linux OSes may be a problem
-                message (STATUS "tring pkg_check_modules: " ${name_})
-
                 include (FindPkgConfig)
                 if (PKG_CONFIG_FOUND)
                     pkg_check_modules(${PREFIX} ${name_})
@@ -164,7 +160,7 @@ macro (sagase_configure_package PREFIX)
                 endif ()
 
                 if (${PREFIX}_FOUND)
-                    message (STATUS "sagase: configured " ${PREFIX})
+                    message (STATUS "sagase: configured " ${PREFIX} " using pkg-config")
                     # already set: ${PREFIX}_INCLUDE_DIRS, 
                     # ${PREFIX}_LIBRARY_DIRS, ${PREFIX}_LIBRARIES
                     set (${PREFIX}_DEFINITIONS ${${PREFIX}_CFLAGS_OTHER})
@@ -180,13 +176,15 @@ macro (sagase_configure_package PREFIX)
     endforeach ()
 
     if (NOT found_)
-        message (STATUS "trying brute-force search ")
+        message (STATUS "sagase: trying brute-force search")
 
         # take names to be directory names, and include "."
-        set (PKG_NAMES ${PKG_NAMES} ".")
+        set (${PREFIX}_PATH_NAMES ${PKG_NAMES} ".")
 
         # generate a combination of possible search paths
-        sagase_generate_paths (include_paths library_paths NAMES ${PKG_NAMES} PREFIXES ${PKG_PREFIXES})
+        sagase_generate_paths (include_paths library_paths 
+            NAMES ${${PREFIX}_PATH_NAMES} 
+            PREFIXES ${PKG_PREFIXES})
 
         # all C and C++ headers
         set (HEADER_POSTFIXES ".h" ".hpp" ".hh" ".hxx")
@@ -202,34 +200,64 @@ macro (sagase_configure_package PREFIX)
             message (FATAL_ERROR "sagase: " ${PREFIX} ": Unable to detect OS type")
         endif ()
 
+        # debug library tags
+        set (LIB_DEBUG_TAGS "d" "_d")
+
         # try using "COMPONENTS" as possible file names (without prefix or extension)
         foreach (component_ ${PKG_COMPONENTS})
             
             # get header path
-            foreach (pkgname_ ${PKG_NAMES})
+            foreach (pathnames_ ${${PREFIX}_PATH_NAMES})
                 foreach (header_extension_ ${HEADER_POSTFIXES})
-                    find_path (${PREFIX}_${component_}_INCLUDE_DIR ${pkgname_}/${component_}${header_extension_} ${include_paths})
+                    find_path (${PREFIX}_${component_}_INCLUDE_DIR 
+                        ${pathnames_}/${component_}${header_extension_} 
+                        ${include_paths})
                     
                     if (${PREFIX}_${component_}_INCLUDE_DIR)
-                        set (${PREFIX}_INCLUDE_DIRS ${${PREFIX}_INCLUDE_DIRS} ${${PREFIX}_${component_}_INCLUDE_DIR})
+                        set (${PREFIX}_INCLUDE_DIRS 
+                            ${${PREFIX}_INCLUDE_DIRS} 
+                            ${${PREFIX}_${component_}_INCLUDE_DIR})
                     endif ()
                 endforeach ()
             endforeach ()
 
             # get library path
             foreach (lib_extension_ ${LIB_POSTFIXES})
-                find_path (${PREFIX}_${component_}_LIBRARY_DIR ${LIB_PREFIX}${component_}${lib_extension_} ${library_paths})
+                find_path (${PREFIX}_${component_}_LIBRARY_DIR 
+                    ${LIB_PREFIX}${component_}${lib_extension_} 
+                    ${library_paths})
 
                 if (${PREFIX}_${component_}_LIBRARY_DIR)
-                    set (${PREFIX}_LIBRARIES ${${PREFIX}_LIBRARIES} ${component_})
-                    set (${PREFIX}_LIBRARY_DIRS ${${PREFIX}_LIBRARY_DIRS} ${${PREFIX}_${component_}_LIBRARY_DIR})
+                    set (${PREFIX}_LIBRARIES 
+                        ${${PREFIX}_LIBRARIES} 
+                        ${component_})
+
+                    set (${PREFIX}_LIBRARY_DIRS 
+                        ${${PREFIX}_LIBRARY_DIRS} 
+                        ${${PREFIX}_${component_}_LIBRARY_DIR})
+
+                    # look for associated debug version
+                    foreach (debug_tag_ ${LIB_DEBUG_TAGS})
+                        find_path (${PREFIX}_${component_}${debug_tag_}_DEBUG_LIBRARY
+                            ${LIB_PREFIX}${component_}${debug_tag_}${lib_extension_} 
+                            ${library_paths})
+
+                        if (${PREFIX}_${component_}${debug_tag_}_DEBUG_LIBRARY)
+                            set (${PREFIX}_DEBUG_LIBRARIES 
+                                ${${PREFIX}_DEBUG_LIBRARIES} 
+                                ${component_}${debug_tag_})
+                        endif ()
+                    endforeach ()
                 endif ()
             endforeach ()
         endforeach ()
     endif ()
 
     # stop process if nothing is found through any means
-    if (NOT found_ AND NOT ${PREFIX}_INCLUDE_DIRS AND NOT ${PREFIX}_LIBRARY_DIRS AND NOT ${PREFIX}_LIBRARIES)
+    if (NOT found_ 
+            AND NOT ${PREFIX}_INCLUDE_DIRS 
+            AND NOT ${PREFIX}_LIBRARY_DIRS 
+            AND NOT ${PREFIX}_LIBRARIES)
         message (FATAL_ERROR "!! sagase: unable to configure " ${PREFIX}) 
     endif ()
     
@@ -246,11 +274,50 @@ macro (sagase_configure_package PREFIX)
         list (REMOVE_DUPLICATES ${PREFIX}_LIBRARIES)
     endif ()
 
-    # report to screen what was found
-    message (STATUS "sagase: " ${PREFIX} " Configure Results.")
-    message (STATUS "-- Include Directories: " ${${PREFIX}_INCLUDE_DIRS})
-    message (STATUS "-- Libarary Directories: " ${${PREFIX}_LIBRARY_DIRS})
-    message (STATUS "-- Libraries: " ${${PREFIX}_LIBRARIES})
-    message (STATUS "-- Defines: " ${${PREFIX}_DEFINITIONS})
+    if (${PREFIX}_DEBUG_LIBRARIES)
+        list (REMOVE_DUPLICATES ${PREFIX}_DEBUG_LIBRARIES)
+    endif ()
 
 endmacro (sagase_configure_package)
+
+# report to screen what was found
+macro (sagase_configure_report PREFIX)
+    message (STATUS "sagase: " ${PREFIX} " Configure Results.")
+	
+    if (${PREFIX}_INCLUDE_DIRS)
+        message (STATUS "-- Include Directories:")
+        foreach (include_dir ${${PREFIX}_INCLUDE_DIRS})
+            message (STATUS "       " ${include_dir})
+        endforeach()
+    endif ()
+	
+    if (${PREFIX}_LIBRARY_DIRS)
+        message (STATUS "-- Libarary Directories:")
+        foreach (library_dir ${${PREFIX}_LIBRARY_DIRS})
+            message (STATUS "       " ${library_dir})
+        endforeach()
+    endif ()
+	
+    if (${PREFIX}_LIBRARIES)
+        message (STATUS "-- Libraries:")
+        foreach (library ${${PREFIX}_LIBRARIES})
+            message (STATUS "       " ${library})
+        endforeach()
+    endif ()
+	
+    if (${PREFIX}_DEBUG_LIBRARIES)
+        message (STATUS "-- Debug Libraries:")
+        foreach (debug_library ${${PREFIX}_DEBUG_LIBRARIES})
+            message (STATUS "       " ${debug_library})
+        endforeach()
+    endif ()
+	
+    if (${PREFIX}_DEFINITIONS)
+        message (STATUS "-- Defines:")
+        foreach (definition ${${PREFIX}_DEFINITIONS})
+            message (STATUS "       " ${definition})
+        endforeach()
+    endif ()
+	
+	message (STATUS "")
+endmacro (sagase_configure_report)
